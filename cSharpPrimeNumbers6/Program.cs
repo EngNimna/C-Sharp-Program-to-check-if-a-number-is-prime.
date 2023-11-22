66f4f9b45e4cda4ba365143a417014e93bc0c87c
nimna nilwala <nimna.nilwala555@gmail.com>
2023-11-22 20:14:30 +0530

    Add project files.

---
+++ cSharpPrimeNumbers6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace cSharpPrimeNumbers6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num,i, flag=0;
+            Console.WriteLine("Enter the number...: ");
+            num = int.Parse(Console.ReadLine());
+
+            if (num > 0)
+            {
+                for (i = 2; i < num; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        flag = 1;
+                    }
+                }
+                if (flag == 0)
+                {
+                    Console.WriteLine(num + " is a PRIME number.");
+                }
+                else
+                {
+                    Console.WriteLine(num + " is not a PRIME number.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a VALID number....!");
+            }
+        }
+    }
+}
